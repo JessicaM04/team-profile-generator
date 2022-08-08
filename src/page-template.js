b5999a35643f02c5325eaece8fc7359b88d587f0
JessicaM04 <jessica.mcnamara84@gmail.com>
2022-08-08 12:23:06 -0400

    file structure, beginning question array

---
+++ src/page-template.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Password Generator</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="card">
+      <img src="" alt="" style="width:100%"></img>
+      <div class="container">
+        
+      </div>
+    </div>
+  </body>
+</html>
